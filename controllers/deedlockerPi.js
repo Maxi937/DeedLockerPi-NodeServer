a0a970af6cc561eadcee47c615834ffc9d5bbbcb
mhornby123@gmail.com <mhornby123@gmail.com>
2022-12-22 23:58:57 +0000

    write boxid to rfid

--- controllers/deedlockerPi.js
+++ controllers/deedlockerPi.js
@@ -2,13 +2,15 @@
 
 const dotenv = require("dotenv");
 const logger = require("../config/logger.js");
-const spawn = require("child_process").spawn;
+const spawn = require('child_process').spawn;
+const exec = require('child_process').exec;
 const fetch = require("node-fetch")
 
 const deedlockerPi = {
 
   ChangeModeRead(req, res) {
     logger.info("Read Mode");
+    const myexec = exec("killall python3");
     var process = spawn("python3", ["/home/pi/DeedLockerPi/read_boxId.py",
       "-u",
       { detached: true, stdio: "ignore" },
@@ -16,15 +18,57 @@ const deedlockerPi = {
     res.render("index");
   },
 
-  ChangeModeWrite(req, res) {
+  async ChangeModeWrite(req, res) {
+    dotenv.config({ path: "./config/config.env" });
     logger.info("Write Mode");
+
+    const myexec = exec("killall python3");
+    const request = await fetch(`${process.env.WEBAPPLOCALURL}${process.env.GETDEEDBOXES}`)
+    const deedboxes = await request.json()
+  
+    const viewData = {
+      deedboxes
+    };
+    res.render("writeRfid", viewData);
+  },
+
+  writeBoxIdToRFID(req, res){
+    logger.info("Writing ID to RFID")
+    const boxId = req.params._id
+
+    // spawn python process to write to the RFID
+    var process = spawn("python3", ["/home/pi/DeedLockerPi/write_boxId.py",
+      boxId,
+      "-u",
+      { detached: true, stdio: "ignore" },
+    ]);
     res.render("index");
+  },
+
+  async updateRfid(req, res){
+    logger.info("Update RFID received from Rasberry Pi")
+
+    if (req.body.code == 200){
+      const data = req.body.data
+      const url = process.env.WEBAPPLOCALURL
+
+      fetch(`${url}/deedlockerPi/updateRfid`, {
+        method: 'post',
+        body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' }
+      });
+      res.sendStatus(200)
+      }
+      else {
+        res.sendStatus(500)
+      }
   },
 
   rpiMessage(req, res) {
     logger.info("Response from RPI");
+  
     if (req.body != "") {
-      console.log(req.body);
+      console.log(req.body)
       res.sendStatus(200);
     }
   },

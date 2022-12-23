74cc387ea64dcd8403ca52f62f3b82eec389720c
Maxi937 <mhornby123@gmail.com>
2022-12-23 19:01:27 +0000

    rfid redirect on write

--- public/scripts/checkPythonProcess.js
+++ public/scripts/checkPythonProcess.js
@@ -1,10 +1,9 @@
 'use strict'
 
 async function checkIfRfidUpdated(boxId) {
-    console.log("Im here")
     let result = await fetch(`http://192.168.1.13:3000/deedlockerPi/checkifRfidUpdated/${boxId}`, { mozSystem: true })
     result = await result.json()
-    console.log(result.deedBox.rfid)
+    return result.deedBox.rfid
 }
 
 
